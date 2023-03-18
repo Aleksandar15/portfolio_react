f75f7ebee96a715563ad1560d53a476cc0a8f8c3
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-03-18 01:35:03 +0100

    Improved  media queries for phone screens; added one extra div wrapper on header so it doesnt affect My Projects sections which has s1 class div wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -57,100 +57,102 @@ function App() {
           />
 
           <section className="s1">
-            <div className="main-container">
-              <div className="greeting-wrapper">
-                <h1>Hi, I'm Aleksandar Angelov</h1>
-              </div>
-
-              <div className="intro-wrapper">
-                <div className="nav-wrapper">
-                  <a href="/">
-                    <div className="dots-wrapper">
-                      <div id="dot-1" className="browser-dot"></div>
-                      <div id="dot-2" className="browser-dot"></div>
-                      <div id="dot-3" className="browser-dot"></div>
-                    </div>
-                  </a>
-
-                  <ul id="navigation">
-                    <li>
-                      <a
-                        className="fa fa-github"
-                        style={{ fontSize: "30px", color: "red" }}
-                        href="https://github.com/Aleksandar15"
-                        target="_blank"
-                        rel="noreferrer"
-                      ></a>
-                    </li>
-                    <li>
-                      <a
-                        className="fa fa-instagram"
-                        style={{ fontSize: "30px", color: "green" }}
-                        href="HTTPS://instagram.com/aleksandarr15"
-                        target="_blank"
-                        rel="noreferrer"
-                      ></a>
-                    </li>
-                    <li>
-                      <a
-                        className="fa fa-linkedin"
-                        style={{
-                          fontSize: "30px",
-                          color: "rgba(0, 0, 255, 0.777)",
-                        }}
-                        href="https://www.linkedin.com/in/aleksandar15"
-                        target="_blank"
-                        rel="noreferrer"
-                      ></a>
-                    </li>
-                    <li>
-                      <a href="#contact">Contact</a>
-                      {/* <a href="#contact" style={{marginLeft:"10px"}}>About</a> */}
-                    </li>
-                  </ul>
+            <div className="s1_header">
+              <div className="main-container">
+                <div className="greeting-wrapper">
+                  <h1>Hi, I'm Aleksandar Angelov</h1>
                 </div>
 
-                <div className="left-column">
-                  <img
-                    id="profile_pic"
-                    src="images/Aleksandar.jpg"
-                    alt="Aleksandar"
-                  />
-                  <h5 className="darkmode" onClick={toggleDarkMode}>
-                    Dark Mode
-                  </h5>
+                <div className="intro-wrapper">
+                  <div className="nav-wrapper">
+                    <a href="/">
+                      <div className="dots-wrapper">
+                        <div id="dot-1" className="browser-dot"></div>
+                        <div id="dot-2" className="browser-dot"></div>
+                        <div id="dot-3" className="browser-dot"></div>
+                      </div>
+                    </a>
 
-                  <div id="theme-options-wrapper">
-                    <div>
-                      {/* <input type="checkbox" className="checkbox" id="chk" onChange={toggleDarkMode} checked={isDarkMode} /> */}
-                      <input
-                        type="checkbox"
-                        className="checkbox"
-                        id="switchDark"
-                        onChange={toggleDarkMode}
-                        checked={darkMode}
-                      />
-                      <label className="label" htmlFor="switchDark">
-                        <i className="fa fa-sun-o"></i>
-                        <i className="fa fa-moon-o"></i>
-                        <div className="ball"></div>
-                      </label>
+                    <ul id="navigation">
+                      <li>
+                        <a
+                          className="fa fa-github"
+                          style={{ fontSize: "30px", color: "red" }}
+                          href="https://github.com/Aleksandar15"
+                          target="_blank"
+                          rel="noreferrer"
+                        ></a>
+                      </li>
+                      <li>
+                        <a
+                          className="fa fa-instagram"
+                          style={{ fontSize: "30px", color: "green" }}
+                          href="HTTPS://instagram.com/aleksandarr15"
+                          target="_blank"
+                          rel="noreferrer"
+                        ></a>
+                      </li>
+                      <li>
+                        <a
+                          className="fa fa-linkedin"
+                          style={{
+                            fontSize: "30px",
+                            color: "rgba(0, 0, 255, 0.777)",
+                          }}
+                          href="https://www.linkedin.com/in/aleksandar15"
+                          target="_blank"
+                          rel="noreferrer"
+                        ></a>
+                      </li>
+                      <li>
+                        <a href="#contact">Contact</a>
+                        {/* <a href="#contact" style={{marginLeft:"10px"}}>About</a> */}
+                      </li>
+                    </ul>
+                  </div>
+
+                  <div className="left-column">
+                    <img
+                      id="profile_pic"
+                      src="images/Aleksandar.jpg"
+                      alt="Aleksandar"
+                    />
+                    <h5 className="darkmode" onClick={toggleDarkMode}>
+                      Dark Mode
+                    </h5>
+
+                    <div id="theme-options-wrapper">
+                      <div>
+                        {/* <input type="checkbox" className="checkbox" id="chk" onChange={toggleDarkMode} checked={isDarkMode} /> */}
+                        <input
+                          type="checkbox"
+                          className="checkbox"
+                          id="switchDark"
+                          onChange={toggleDarkMode}
+                          checked={darkMode}
+                        />
+                        <label className="label" htmlFor="switchDark">
+                          <i className="fa fa-sun-o"></i>
+                          <i className="fa fa-moon-o"></i>
+                          <div className="ball"></div>
+                        </label>
+                      </div>
                     </div>
                   </div>
-                </div>
 
-                <div className="right-column">
-                  <div id="preview-shadow">
-                    <div id="preview">
-                      <div id="corner-tl" className="corner"></div>
-                      <div id="corner-tr" className="corner"></div>
-                      <h3>What I Do</h3>
-                      <p>
-                        I am a Full-Stack Web Developer. <br />I would be glad
-                        to work with your team!
-                      </p>
-                      <div id="corner-br" className="corner"></div>
-                      <div id="corner-bl" className="corner"></div>
+                  <div className="right-column">
+                    <div id="preview-shadow">
+                      <div id="preview">
+                        <div id="corner-tl" className="corner"></div>
+                        <div id="corner-tr" className="corner"></div>
+                        <h3>What I Do</h3>
+                        <p>
+                          I am a Full-Stack Web Developer. <br />I would be glad
+                          to work with your team!
+                        </p>
+                        <div id="corner-br" className="corner"></div>
+                        <div id="corner-bl" className="corner"></div>
+                      </div>
                     </div>
                   </div>
                 </div>
